pp3345/gnome-with-patches

Make s390x builds pass

mutter.spec
@@ -22,6 +22,9 @@ Patch0:        0001-window-actor-Special-case-shaped-Java-windows.patch
 # Backports after 3.38.1 (2020-11-17) before 3.38.2
 Patch1:        mutter-3.38.1~17e9cbe8cbee.patch
 
+# To make s390x build pass
+Patch2:        0001-Revert-build-Do-not-provide-built-sources-as-libmutt.patch
+
 BuildRequires: chrpath
 BuildRequires: pango-devel
 BuildRequires: startup-notification-devel

grigorig/mutter-performance

mutter.spec
@@ -19,6 +19,9 @@ Source0:       http://download.gnome.org/sources/%{name}/40/%{name}-40.beta.tar.
 # Work-around for OpenJDK's compliance test
 Patch0:        0001-window-actor-Special-case-shaped-Java-windows.patch
 
+# To make s390x build pass
+Patch1:        0001-Revert-build-Do-not-provide-built-sources-as-libmutt.patch
+
 BuildRequires: chrpath
 BuildRequires: pango-devel
 BuildRequires: startup-notification-devel